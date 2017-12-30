00097ce036acbb21441749f6d23a669874c5154f
杨攀 <yangpan@baixing.com>
2017-12-30 15:30:21 +0800

    add index

--- js/animate.js
+++ js/animate.js
@@ -6,7 +6,15 @@ export const config =  [
         name: 'play-btn' ,
         cls: 'animated infinite pulse'    
     }], 
-    [
+    [{
+        name: 'f1',
+        cls: 'animated fadeIn delay2'
+    },
+    {
+        name: 'f2',
+        cls: 'animated fadeIn delay1'
+    }
+
         //第二页动画
     ], [
         //第三页动画
