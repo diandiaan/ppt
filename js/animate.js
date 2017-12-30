3bf48c54f09fbf52c0359f22f760293e8c63b322
杨攀 <yangpan@baixing.com>
2017-12-30 13:49:56 +0800

    first

---
+++ js/animate.js
@@ -0,0 +1,9 @@
+export const config =  [
+    [{
+        name: 'title',
+        cls: 'animated fadeInLeft'
+    }, {
+        name: 'play-btn' ,
+        cls: 'animated infinite pulse'    
+    }]
+] 
